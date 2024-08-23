fad1f591c86cbe263b51d5aad10973b8601096d2
Shridhar Shinde <shindeshridhav@gmail.com>
2024-08-23 15:45:22 -0700

    cloudwatch-issues-fix

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "main" {
   count                  = var.instance_count
   ami                    = var.ami
   instance_type          = var.instance_type
-#   key_name               = var.ssh_key_name
   vpc_security_group_ids = var.security_group_ids
   subnet_id              = var.subnet_ids[count.index % length(var.subnet_ids)]
-
+  iam_instance_profile = var.iam_instance_profile
+  user_data = var.user_data
   tags = {
     Name = "${var.environment}-ec2-${count.index + 1}"
   }
